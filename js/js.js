eac6b3429916d938a4d1b5a9d608e6ef3f03c0fb
denislav <58664415+denislavmarinov@users.noreply.github.com>
2020-07-03 11:54:20 +0300

    Update js.js
    Add some more logic. The output is not exactly correct.

--- js/js.js
+++ js/js.js
@@ -1,4 +1,4 @@
-function game(arr) {
+function game(arr){
 	let num = 0, targets = [], bombThrowPlaces = []; 
 	for (let i = 0; i < arr.length; i++) {
 		bombThrowPlaces[num] = {}; 
@@ -9,8 +9,37 @@ function game(arr) {
 			}
 		}
 	}
-}
 
+	num = 0;
+
+	for (ind in bombThrowPlaces) {
+		let x = bombThrowPlaces[ind].row;
+		
+		targets[num] = {targetsCount: 0};
+
+		for (let y = 0; y < arr.length; y++) {
+			if (arr[x][y] == "W") {
+				break;
+			} else if (arr[x][y] == "E") {
+				targets[num].targetsCount += 1;
+			}
+		}
+
+		y = bombThrowPlaces[ind].col;
+		x = 0;
+		
+		for (x = 0; x < arr.length; x++) {
+			if (arr[x][y] == "W") {
+				break;
+			} else if (arr[x][y] == "E") {
+				targets[num].targetsCount += 1;
+			}
+		}
+		num++;
+	}
+	console.log(bombThrowPlaces);
+	console.log(targets);
+}
 
 let input = [
 	["0", "0", "E", "0"],
@@ -20,4 +49,4 @@ let input = [
 ];
 
 
-console.log(game(input));
+game(input);
