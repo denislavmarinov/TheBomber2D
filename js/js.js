17fe53bb0160f8df396aa2d93481681a399fb589
denislav <58664415+denislavmarinov@users.noreply.github.com>
2020-07-23 10:39:56 +0300

    Code optimization

--- js/js.js
+++ js/js.js
@@ -1,306 +1,157 @@
-function bombPlaceCasesCheck (arr, x, y, loopEnd, ifArg1, ifArg2, varToChange) {
+function bombPlaceCasesCheck(arr, x, y, num, targets, loopEnd, ifArg1, ifArg2, varToChange) {
+    let rowNum = x,
+        colNum = y, arg1New;
 
-	for (let k = 0; k < loopEnd; k++) {
-		if (ifArg1 == (ifArg2)) {
-			break;
-		}
-		if (arr[x][y] == "W") {
-			break;
-		} else if (arr[x][y] == "E") {
-			targets[num].targetsCount += 1;
-		}
-		varToChange;
-	}
-}
-function game(arr){
-	var num = 0, targets = [];
-	let bombThrowPlaces = []; 
-	// Find the empty ( 0 ) spaces in the input
-	for (let i = 0; i < arr.length; i++) {
-		for (let j = 0; j < arr[i].length; j++) {
-			if(arr[i][j] == "0"){
-				bombThrowPlaces[num] = {};
-				bombThrowPlaces[num] = {row: i, col: j}; 
-				num++;
-			}
-		}
-	}
-	num = 0;
-	// Check every case in which the bomb can be planted somewhere
-	for (let ind in bombThrowPlaces) {
-		let x = bombThrowPlaces[ind].row, y = bombThrowPlaces[ind].col;
-
-		targets[num] = {targetsCount: 0};
-		// Search for rnrmy in a row (form let to rigth) => x is not changing
-		bombPlaceCasesCheck(arr, x, y, arr[0].length, y, arr[0].length, y++);
-
-
-		// for (let k = 0; k < arr[0].length; k++) {
-		// 	// Check if y is equal to the end of the row and make it 0 (zero)
-		// 	if (y == (arr[0].length)) {
-		// 		break;
-		// 	}
-		// 	if (arr[x][y] == "W") {
-		// 		break;
-		// 	} else if (arr[x][y] == "E") {
-		// 		targets[num].targetsCount += 1;
-		// 	}
-		// 	y++;
-		// }
-
-		// Set the value again so not to be changed after the previous activities with them
-		x = bombThrowPlaces[ind].row, y = bombThrowPlaces[ind].col;
-
-		// Searching for enemy in a cols (form top to down) => y is not changing.
-		bombPlaceCasesCheck(arr, x, y, arr.length, x, arr.length, x++);
-		// for (let l = 0; l < arr.length; l++) {
-		// 	// Check if x is equal to the end of the col and make it 0 (zero)
-		// 	if (x == (arr.length)) {
-		// 		break;
-		// 	}
-		// 	if (arr[x][y] == "W") {
-		// 		break;
-		// 	} else if (arr[x][y] == "E") {
-		// 		targets[num].targetsCount += 1;
-		// 	}
-		// 	x++;
-		// }
-		// Set the value again so not to be changed after the previous activities with them
-		x = bombThrowPlaces[ind].row, y = bombThrowPlaces[ind].col;
-
-		// Search for rnrmy in a row (form rigth to left) => x is not changing
-		bombPlaceCasesCheck(arr, x, y, arr[0].length, y, -1, y--);
-		// for (let m = 0; m < arr[0].length; m++) {
-		// 	// Check if y is equal to the end of the row and make it 0 (zero)
-		// 	if (y == -1) {
-		// 		break;
-		// 	}
-		// 	if (arr[x][y] == "W") {
-		// 		break;
-		// 	} else if (arr[x][y] == "E") {
-		// 		targets[num].targetsCount += 1;
-		// 	}
-		// 	y--;
-		// }
-
-		// Set the value again so not to be changed after the previous activities with them
-		x = bombThrowPlaces[ind].row, y = bombThrowPlaces[ind].col;
-		
-		// Searching for enemy in a cols (form down to top) => y is not changing
-		bombPlaceCasesCheck(arr, x, y, arr.length, x,  -1, x--);
-		// for (let h = 0; h < arr.length; h++) {
-		// 	// Check if x is equal to the end of the col and make it 0 (zero)
-		// 	if (x == -1) {
-		// 		break;
-		// 	}
-		// 	if (arr[x][y] == "W") {
-		// 		break;
-		// 	} else if (arr[x][y] == "E") {
-		// 		targets[num].targetsCount += 1;
-		// 	}
-		// 	x--;
-		// }
-
-		num++;
-	}
-
-	// Find the biggest number of died enemies
-	let mostTargetsDown = [], maxTargetsDown = 0, maxTargetsDownArr = [];
-	num = 0;
-
-	// Store the values in array
-	for (let o = 0; o < targets.length; o++) {
-		maxTargetsDownArr.push(targets[o].targetsCount);
-	}
-
-	maxTargetsDown = Math.max(...maxTargetsDownArr);
-
-	// Search for biggest number/s of died enemies
-	for (let g = 0; g < targets.length; g++) {
-		if (targets[g].targetsCount == maxTargetsDown) {
-			mostTargetsDown[num] = {
-				value: targets[g].targetsCount,
-				rowToDrop: bombThrowPlaces[g].row,
-				colToDrop: bombThrowPlaces[g].col
-			};
-			num++;
-		}
-	}
-
-	return mostTargetsDown;
+    for (let k = 0; k < loopEnd; k++) {
+        if (ifArg1 == "x") {
+            ifArg1New = rowNum;
+        } else if (ifArg1 == "y") {
+            ifArg1New = colNum;
+        }
+        if ((+ifArg1New) == (+ifArg2)) {
+            break;
+        }
+        // console.log(rowNum, colNum);
+        // console.log(arr[rowNum][colNum]);
+        if (arr[rowNum][colNum] == "W") {
+            break;
+        } else if (arr[rowNum][colNum] == "E") {
+            targets[num].targetsCount += 1;
+        }
+        switch (varToChange) {
+            case 0:
+                colNum++;
+                break;
+            case 1:
+                rowNum++;
+                break;
+            case 2:
+                colNum--;
+                break;
+            case 3:
+                rowNum--;
+                break;
+        }
+    }
+    return targets;
 }
 
-// let input = [
-// 	["0", "0", "E", "0"],
-// 	["W", "0", "W", "E"],
-// 	["0", "E", "0", "W"],
-// 	["0", "W", "0", "E"]
-// ];
+function game(arr) {
+    let num = 0, targets = [], bombThrowPlaces = [];
+    // Find the empty ( 0 ) spaces in the input
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] == "0") {
+                bombThrowPlaces[num] = {};
+                bombThrowPlaces[num] = { row: i, col: j };
+                num++;
+            }
+        }
+    }
+    num = 0;
+    // Check every case in which the bomb can be planted somewhere
+    for (let ind in bombThrowPlaces) {
 
-// let input = [
-// 	["0", "E", "0", "0"],
-// 	["E", "0", "W", "E"],
-// 	["0", "E", "0", "0"]
-// ];
+        targets[num] = { targetsCount: 0 };
 
+        // Search for rnrmy in a row (form let to rigth) => x is not changing
+        bombPlaceCasesCheck(arr, bombThrowPlaces[ind].row, bombThrowPlaces[ind].col, num, targets, arr[0].length, "y", arr[0].length, 0);
 
-// let input = [
-// 	["0", "E"]
-// ];
+        // Searching for enemy in a cols (form top to down) => y is not changing.
+        bombPlaceCasesCheck(arr, bombThrowPlaces[ind].row, bombThrowPlaces[ind].col, num, targets, arr.length, "x", arr.length, 1);
 
-// let input = [
-// 	["0", "E", "0", "W"]
-// ];
+        // Search for rnrmy in a row (form rigth to left) => x is not changing
+        bombPlaceCasesCheck(arr, bombThrowPlaces[ind].row, bombThrowPlaces[ind].col, num, targets, arr[0].length, "y", -1, 2);
 
-// let input = [
-// 	["W"],
-// 	["E"],
-// 	["W"],
-// 	["0"],
-// 	["E"]
-// ]
+        // Searching for enemy in a cols (form down to top) => y is not changing
+        bombPlaceCasesCheck(arr, bombThrowPlaces[ind].row, bombThrowPlaces[ind].col, num, targets, arr.length, "x", -1, 3);
 
-// let input = [
-// 	["0", "W", "E", "w"]
-// ];
+        num++;
+    }
 
-// let input = [
-// 	["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-// 	["E", "E", "E", "E", "E", "E", "E", "E", "E", "E"]
-// ];
+    // Find the biggest number of died enemies
+    let mostTargetsDown = [],
+        maxTargetsDown = 0,
+        maxTargetsDownArr = [];
+    num = 0;
 
-// let input = [
-// 	["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-// 	["E", "E", "E", "E", "E", "E", "E", "E", "E", "E"],
-// 	["W", "W", "W", "W", "W", "W", "W", "W", "W", "W"]
-// ];
+    // Store the values in array
+    for (let o = 0; o < targets.length; o++) {
+        maxTargetsDownArr.push(targets[o].targetsCount);
+    }
 
-// let input = [
-// 	["E", "E", "E", "E", "E", "E", "E", "E", "E", "E"],
-// 	["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-// 	["E", "E", "E", "E", "E", "E", "E", "E", "E", "E"]
-// ]
+    maxTargetsDown = Math.max(...maxTargetsDownArr);
 
-// let input = [
-// 	["0", "E", "0", "0"],
-// 	["E", "0", "W", "E"],
-// 	["E", "0", "W", "0"]
-// ];
+    // Search for biggest number/s of died enemies
+    for (let g = 0; g < targets.length; g++) {
+        if (targets[g].targetsCount == maxTargetsDown) {
+            mostTargetsDown[num] = {
+                value: targets[g].targetsCount,
+                rowToDrop: bombThrowPlaces[g].row,
+                colToDrop: bombThrowPlaces[g].col
+            };
+            num++;
+        }
+    }
 
-// let input = [
-// 	["W", "W", "W", "W"],
-// 	["W", "0", "W", "E"],
-// 	["E", "W", "0", "W"]
-// ];
-
-// let input = [
-// 	["0", "E", "E", "0"],
-// 	["E", "W", "W", "E"],
-// 	["E", "W", "W", "E"],
-// 	["0", "E", "E", "0"]
-// ]
-
-// let input = [];
-
-// let input = [[]];
-
-// let input =[
-// 	["E"],
-// 	["E"],
-// 	["E"]
-// ];
-
-// let input = [
-// 	["0"],
-// 	["W"],
-// 	["0"]
-// ]
-
-// let input = [
-// 	["0"],
-// 	["E",  "W"]
-// ]
-
-let input = [
-	["P"],
-	["E"],
-	["W"],
-	["0"]
-]
-
-//console.log(game(input));
-
-
-function checkIfInputArrIsEmpty(arr){
-	if (arr.length == 0) {
-		return false;
-	}else{
-		return true;
-	}
+    return mostTargetsDown;
 }
 
-// console.log(checkIfInputArrIsEmpty(input));
-
-
-function checkIfInputSubArrIsEmpty (arr) {
-	if (arr[0].length == 0) {
-		return false;
-	}else{
-		return true;
-	}
+function checkIfInputArrIsEmpty(arr) {
+    if (arr.length == 0) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
-// console.log(checkIfInputSubArrIsEmpty(input));
-
-
-function checkIfInputDoesNotContain0 (arr) {
-	for (let ii = 0; ii < arr.length; ii++) {
-		if (arr[ii].includes("0")) {
-			return true;
-		}
-	}
-	return false;
+function checkIfInputSubArrIsEmpty(arr) {
+    if (arr[0].length == 0) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
-// console.log(checkIfInputDoesNotContain0(input));
-
-function checkIfInputDoesNotContainEnemies (arr) {
-	for (let ij = 0; ij < arr.length; ij++) {
-		if (arr[ij].includes("E")) {
-			return true;
-		}
-	}
-	return false;
+function checkIfInputDoesNotContain0(arr) {
+    for (let ii = 0; ii < arr.length; ii++) {
+        if (arr[ii].includes("0")) {
+            return true;
+        }
+    }
+    return false;
 }
 
-// console.log(checkIfInputDoesNotContainEnemies(input));
-
-
-function checkIfInputSubArrLengthIsNotEqual(arr){
-	let lengthsOfSubarr = [];
-	for (let ik = 0; ik < arr.length; ik++) {
-		lengthsOfSubarr.push(arr[ik].length);
-	}
-
-	for (let il = 0; il < lengthsOfSubarr.length-1; il++) {
-		if (lengthsOfSubarr[il] != lengthsOfSubarr[il+1]) {
-			return false;
-		}
-	}
-	return true;
+function checkIfInputDoesNotContainEnemies(arr) {
+    for (let ij = 0; ij < arr.length; ij++) {
+        if (arr[ij].includes("E")) {
+            return true;
+        }
+    }
+    return false;
 }
 
-// console.log(checkIfInputSubArrLengthIsNotEqual(input));
+function checkIfInputSubArrLengthIsNotEqual(arr) {
+    let lengthsOfSubarr = [];
+    for (let ik = 0; ik < arr.length; ik++) {
+        lengthsOfSubarr.push(arr[ik].length);
+    }
 
-function checkIfTheInputIsCorrect(arr){
-	for (let im = 0; im < arr.length; im++) {
-		for (var ip = 0; ip < arr[im].length; ip++) {
-			if (arr[im][ip] != String.fromCharCode(69) && arr[im][ip] != String.fromCharCode(48) && arr[im][ip] != String.fromCharCode(87)) {
-				return false;
-			}
-		}
-	}
-	return true;
+    for (let il = 0; il < lengthsOfSubarr.length - 1; il++) {
+        if (lengthsOfSubarr[il] != lengthsOfSubarr[il + 1]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-// console.log(checkIfTheInputIsCorrect(input));
+function checkIfTheInputIsCorrect(arr) {
+    for (let im = 0; im < arr.length; im++) {
+        for (var ip = 0; ip < arr[im].length; ip++) {
+            if (arr[im][ip] != String.fromCharCode(69) && arr[im][ip] != String.fromCharCode(48) && arr[im][ip] != String.fromCharCode(87)) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
